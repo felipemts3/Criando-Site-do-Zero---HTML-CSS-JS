cd2313d04cbf668c367e7f6bd2b4fa21c6d019ad
felipemts3 <flipesantos33@gmail.com>
2020-10-28 10:08:40 -0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-$('.slider-principal').slick({
-    dots: true,
-    infinite: true,
-    speed: 800,
-    slidesToShow: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    adaptiveHeight: true
-});
